a43d858ccfb531d0596f1fd39623516469e31e57
Marcin Przywóski <bluza26@gmail.com>
2021-06-23 18:34:41 +0200

    Added HtmlFile class responsible for storing information about processed HtmlFiles including Messages from Facebook export

---
+++ FacebookExportDatePhotoFixer/Data/HtmlFile.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace FacebookExportDatePhotoFixer.Data
+{
+    class HtmlFile
+    {
+        //private HtmlDocument htmlDocument;
+
+        public string Location { get; }
+        public int MessagesCount { get { return ListOfMessages.Count; } }
+        public List<Message> ListOfMessages { get; } = new List<Message>();
+
+        public HtmlFile(string path)
+        {
+            //this.htmlDocument.Load(path);
+            this.Location = path;
+        }
+        
+    }
+}
