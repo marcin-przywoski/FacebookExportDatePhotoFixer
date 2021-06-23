49ee7df42213a40f4ef0486551d638cac5c5b481
Marcin Przywóski <bluza26@gmail.com>
2021-06-23 18:33:07 +0200

    Added Message class for storing information about messages in exported data from Facebook

---
+++ FacebookExportDatePhotoFixer/Data/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookExportDatePhotoFixer.Data
+{
+    class Message
+    {
+        //public string Sender { get; }
+
+        public DateTime Date { get; }
+
+        public string Link { get; }
+
+        public string ExtensionOfAttachment { get; }
+
+        public Message(DateTime date, string link)
+        {
+            //this.Sender = sender;
+            this.Date = date;
+            this.Link = link;
+            this.ExtensionOfAttachment = link.Substring(link.Length - 4);
+        }
+    }
+}
