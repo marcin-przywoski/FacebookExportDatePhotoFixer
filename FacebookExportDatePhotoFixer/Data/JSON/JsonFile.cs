a5e1f458d3cac19c9739fb1617ec8766baecb630
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2022-09-12 13:15:00 +0200

    Modified using statements to reflect change on the namespace of Conversation class

--- FacebookExportDatePhotoFixer/Data/JSON/JsonFile.cs
+++ FacebookExportDatePhotoFixer/Data/JSON/JsonFile.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using FacebookExportDatePhotoFixer.Data.JSON.Entities;
 using FacebookExportDatePhotoFixer.Interfaces;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace FacebookExportDatePhotoFixer.Data.JSON
 {
-    public class JsonFile : IExportFile
+    public class JsonFile
     {
         public string Location { get; }
 
