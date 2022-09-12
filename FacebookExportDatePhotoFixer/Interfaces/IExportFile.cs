adf66ea7da38e520b70f8bd3ccec4bcd7572d606
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2022-09-12 13:13:50 +0200

    Removed unnecessary usings

--- FacebookExportDatePhotoFixer/Interfaces/IExportFile.cs
+++ FacebookExportDatePhotoFixer/Interfaces/IExportFile.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FacebookExportDatePhotoFixer.Data.JSON;
-using static FacebookExportDatePhotoFixer.Data.JSON.Conversation;
 
 namespace FacebookExportDatePhotoFixer.Interfaces
 {
