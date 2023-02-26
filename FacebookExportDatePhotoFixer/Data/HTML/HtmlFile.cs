18c0455a3cb5faf64fe5b8eae5de2a7fe46fc756
Marcin Przywóski <35424311+marcin-przywoski@users.noreply.github.com>
2023-02-26 22:18:37 +0100

    Removed HtmlAgilityPack reference from HtmlFile.cs as it was blocking compilation

--- FacebookExportDatePhotoFixer/Data/HTML/HtmlFile.cs
+++ FacebookExportDatePhotoFixer/Data/HTML/HtmlFile.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Threading;
-using HtmlAgilityPack;
 
 namespace FacebookExportDatePhotoFixer.Data.HTML
 {
